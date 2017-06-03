ef427fa27b185bc3b8d83900ea2b7aeef93e7e57
Michael Romero <romerom@gmail.com>
2017-06-02 23:41:19 -0700

    python conventions, refactoring

--- rbac.py
+++ rbac.py
@@ -14,79 +14,86 @@ def usage(script_name):
     sys.exit(msg)
 
 
-def populate_resources(resource_file):
-    # Parse the resourceFile and create a data structure to represent it
-    resources = []
-    with open(resource_file, "r") as resourcesFromFile:
-        resource_entries = resourcesFromFile.read().split("\n\n")
-        for resourceEntry in resource_entries:
-            resource_object_subject_perms = resourceEntry.split("\n")
-            resource_object = resource_object_subject_perms[0].split(":")[0]
-            resource_subjects_and_perms = [x.strip(' ').replace(' ', '') for x in resource_object_subject_perms[1:]]
-            subject_and_perms = []
-            for resourceSubjectAndPerms in resource_subjects_and_perms:
-                group_perms = resourceSubjectAndPerms.split(":")
-                perms = group_perms[1].split(",")
-                subject_and_perms.append([group_perms[0], perms])
-            resources.append([resource_object, subject_and_perms])
-    return resources
+def fail_gracefully(e, failed_file):
+    print "I/O error({0}): {1}, {2}".format(e.errno, e.strerror, failed_file)
+    sys.exit(1)
 
 
 def populate_groups(group_file):
-    # Parse the groupFile and create a data structure to represent it
-    groups = []
-    with open(group_file, "r") as groupsFromFile:
-        for line in groupsFromFile:
-            line = line.strip().replace(' ', '')
-            groups_users = line.split(":")
-            groups.append([groups_users[0], groups_users[1].strip().split(",")])
+    groups = []  # Parse the groupFile and create a data structure to represent it
+    try:
+        with open(group_file, "r") as groupsFromFile:
+            for line in groupsFromFile:
+                line = line.strip().replace(' ', '')
+                groups_users = line.split(":")
+                groups.append([groups_users[0], groups_users[1].strip().split(",")])
+    except IOError as e:
+        fail_gracefully(e, group_file)
     return groups
 
 
-def populate_required_membership(action, resource, resources):
-    # We populate "action_to_resource_allowed_by_members_of" array with groups authorized to perform action on
-    action_to_resource_allowed_by_members_of = []
-    for resourceIndex, groupsPrivsOfResource in enumerate(resources):
-        if groupsPrivsOfResource[0] == resource:
-            for group, privileges in groupsPrivsOfResource[1]:
-                if action in privileges:
-                    action_to_resource_allowed_by_members_of.append(group)
-    return action_to_resource_allowed_by_members_of
+def populate_resources(resource_file):
+    resources = []  # Parse the resourceFile and create a data structure to represent it
+    try:
+        with open(resource_file, "r") as resourcesFromFile:
+            resource_entries = resourcesFromFile.read().split("\n\n")
+            for resourceEntry in resource_entries:
+                resource_object_subject_perms = resourceEntry.split("\n")
+                resource_object = resource_object_subject_perms[0].split(":")[0]
+                resource_subjects_and_perms = [x.strip(' ').replace(' ', '') for x in resource_object_subject_perms[1:]]
+                subject_and_perms = []
+                for resourceSubjectAndPerms in resource_subjects_and_perms:
+                    group_perms = resourceSubjectAndPerms.split(":")
+                    perms = group_perms[1].split(",")
+                    subject_and_perms.append([group_perms[0], perms])
+                resources.append([resource_object, subject_and_perms])
+    except IOError as e:
+        fail_gracefully(e, resource_file)
+    return resources
 
 
 def populate_membership(groups, subject):
-    # We populate "subject_is_member_of" array with groups the subject belongs to
-    subject_is_member_of = []
+    membership = []  # Populate "membership" array with groups the subject belongs to
     for groupIndex, userList in enumerate(groups):
         if subject in userList[1]:
-            subject_is_member_of.append(userList[0])
-    return subject_is_member_of
+            membership.append(userList[0])
+    return membership
+
+
+def populate_required_membership(action, resource, resources):
+    action_allowed_by = [] # We populate "action_allowed_by" array with groups authorized to perform action on
+    for resourceIndex, resource_groups_privileges in enumerate(resources):
+        if resource_groups_privileges[0] == resource:
+            for group, privileges in resource_groups_privileges[1]:
+                if action in privileges:
+                    action_allowed_by.append(group)
+    return action_allowed_by
 
 
 def main(group_file, resource_file, action_file):
-    groups = populate_groups(group_file)
-    resources = populate_resources(resource_file)
+    group_data = populate_groups(group_file)
+    resource_data = populate_resources(resource_file)
+    try:
+        with open(action_file, "r") as actionsFromFile:  # Test permission data against attempted actions
+            action_entries = actionsFromFile.read().split("\n")
+            for actionEntry in action_entries:
+                subject_action_resource = actionEntry.split()  # alice, read, /home/alice/
+                subject = subject_action_resource[0]
+                action = subject_action_resource[1]
+                resource = subject_action_resource[2]
+                subject_is_member_of = populate_membership(group_data, subject)
+                action_allowed_by = populate_required_membership(action, resource, resource_data)
 
-    # Now that we've populated our groups and resources permission data, test them against attempted actions
-    with open(action_file, "r") as actionsFromFile:
-        action_entries = actionsFromFile.read().split("\n")
-        for actionEntry in action_entries:
-            subject_action_resource = actionEntry.split() # alice, read, /home/alice/
-            subject = subject_action_resource[0]
-            action = subject_action_resource[1]
-            resource = subject_action_resource[2]
-            subject_is_member_of = populate_membership(groups, subject)
-            action_to_resource_allowed_by_members_of = populate_required_membership(action, resource, resources)
-
-            # Compare the groups our subject is a member of to the groups allowed to perform the action on resource
-            if [i for i in subject_is_member_of if i in action_to_resource_allowed_by_members_of]:
-                print "ALLOW " + subject + " " + action + " " + resource
-            else:
-                print "DENY " + subject + " " + action + " " + resource
+                # If subject is member of a group with permissions to perform action on resource...
+                if [i for i in subject_is_member_of if i in action_allowed_by]:
+                    print "ALLOW " + subject + " " + action + " " + resource
+                else:
+                    print "DENY " + subject + " " + action + " " + resource
+    except IOError as e:
+        fail_gracefully(e, action_file)
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
         usage(sys.argv[0])
-
     main(sys.argv[1], sys.argv[2], sys.argv[3])
